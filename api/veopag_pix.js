d4c17a20c7a630bdb6f2e98b40645849d192937c
AllcanceAI <gabrielalvestrabalhotrader@gmail.com>
2026-07-26 17:00:33 -0300

    Convert serverless functions to CommonJS format for universal Vercel Node compatibility

--- api/veopag_pix.js
+++ api/veopag_pix.js
@@ -47,7 +47,7 @@ async function getVeoPagToken() {
     return null;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // Enable CORS
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
